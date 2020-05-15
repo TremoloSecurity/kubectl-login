5b58ba4bfafb5904a9bbd67013b6dd14f80aa335
mlbiam <marc@tremolo.io>
2020-05-15 17:17:58 -0400

    initial working version

---
+++ go.mod
@@ -0,0 +1,25 @@
+module main
+
+go 1.14
+
+require (
+	cloud.google.com/go v0.57.0 // indirect
+	github.com/Azure/go-autorest/autorest v0.10.1 // indirect
+	github.com/coreos/go-oidc v2.2.1+incompatible
+	github.com/google/btree v1.0.0 // indirect
+	github.com/gophercloud/gophercloud v0.11.0 // indirect
+	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
+	github.com/imdario/mergo v0.3.9 // indirect
+	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
+	github.com/pkg/browser v0.0.0-20180916011732-0a3d74bf9ce4
+	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
+	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37 // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	golang.org/x/time v0.0.0-20200416051211-89c76fbcd5d1 // indirect
+	gopkg.in/square/go-jose.v2 v2.5.1 // indirect
+	k8s.io/api v0.18.2 // indirect
+	k8s.io/apimachinery v0.18.2 // indirect
+	k8s.io/client-go v0.0.0-20200515122352-806d15d65454
+	k8s.io/utils v0.0.0-20200414100711-2df71ebbae66 // indirect
+	sigs.k8s.io/structured-merge-diff v1.0.2 // indirect
+)
---
+++ kubectl-login.go
@@ -0,0 +1,253 @@
+/*
+Copyright 2020 Tremolo Security, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+
+	oidc "github.com/coreos/go-oidc"
+	"github.com/pkg/browser"
+	"golang.org/x/oauth2"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/clientcmd/api"
+)
+
+type oidcService struct {
+	ctx        context.Context
+	provider   *oidc.Provider
+	clientid   string
+	issuer     string
+	verifier   *oidc.IDTokenVerifier
+	config     oauth2.Config
+	state      string
+	httpServer *http.Server
+}
+
+func main() {
+	oidc := &oidcService{
+		clientid: "cli-local",
+		issuer:   "https://k8sou.apps.192-168-2-144.nip.io/auth/idp/k8s-login-cli",
+	}
+
+	browser.OpenURL("https://k8sou.apps.192-168-2-144.nip.io/cli-login")
+
+	m := http.NewServeMux()
+	m.HandleFunc("/", oidc.oidcStartLogin)
+	m.HandleFunc("/redirect", oidc.oidcHandleRedirect)
+
+	oidc.httpServer = &http.Server{Addr: "127.0.0.1:8400", Handler: m}
+
+	oidc.httpServer.ListenAndServe()
+
+}
+
+func (oidcSvc *oidcService) oidcStartLogin(w http.ResponseWriter, r *http.Request) {
+	var err error
+	oidcSvc.ctx = context.Background()
+
+	oidcSvc.provider, err = oidc.NewProvider(oidcSvc.ctx, oidcSvc.issuer)
+	if err != nil {
+		panic(err)
+	}
+
+	oidcConfig := &oidc.Config{
+		ClientID: oidcSvc.clientid,
+	}
+
+	oidcSvc.verifier = oidcSvc.provider.Verifier(oidcConfig)
+
+	oidcSvc.config = oauth2.Config{
+		ClientID:    oidcSvc.clientid,
+		Endpoint:    oidcSvc.provider.Endpoint(),
+		RedirectURL: "http://127.0.0.1:8400/redirect",
+		Scopes:      []string{oidc.ScopeOpenID, "profile", "email"},
+	}
+
+	oidcSvc.state = "foobar" // Don't do this in production.
+
+	http.Redirect(w, r, oidcSvc.config.AuthCodeURL(oidcSvc.state), http.StatusFound)
+
+}
+
+func (oidcSvc *oidcService) oidcHandleRedirect(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("state") != oidcSvc.state {
+		http.Error(w, "state did not match", http.StatusBadRequest)
+		return
+	}
+
+	oauth2Token, err := oidcSvc.config.Exchange(oidcSvc.ctx, r.URL.Query().Get("code"))
+	if err != nil {
+		http.Error(w, "Failed to exchange token: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	rawIDToken, ok := oauth2Token.Extra("id_token").(string)
+	if !ok {
+		http.Error(w, "No id_token field in oauth2 token.", http.StatusInternalServerError)
+		return
+	}
+	idToken, err := oidcSvc.verifier.Verify(oidcSvc.ctx, rawIDToken)
+	if err != nil {
+		http.Error(w, "Failed to verify ID Token: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte(idToken.Subject))
+
+	//fmt.Println(rawIDToken)
+
+	httpReq, err := http.NewRequest("GET", "https://k8sou.apps.192-168-2-144.nip.io/k8slogin/token/user", nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	httpReq.Header.Add("Authorization", rawIDToken)
+
+	httpResp, err := http.DefaultClient.Do(httpReq)
+
+	if err != nil {
+		panic(err)
+	}
+
+	data, err := ioutil.ReadAll(httpResp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(data))
+
+	var objmap map[string]json.RawMessage
+	err = json.Unmarshal(data, &objmap)
+	fmt.Println(string(objmap["displayName"]))
+	fmt.Println(objmap["token"])
+	var tokenmap map[string]json.RawMessage
+	err = json.Unmarshal(objmap["token"], &tokenmap)
+	fmt.Println(tokenmap)
+
+	userName := byte2string(objmap["displayName"])
+	k8sURL := byte2string(tokenmap["kubectl Windows Command"])
+	ctxName := byte2string(tokenmap["kubectl Command"])
+	refreshToken := byte2string(tokenmap["refresh_token"])
+	userIDToken := byte2string(tokenmap["id_token"])
+
+	var ouCert, k8sCert string
+
+	data, ok = tokenmap["OpenUnison Server CA Certificate"]
+	if ok {
+		ouCert = base64.StdEncoding.EncodeToString([]byte(byte2string(data)))
+	} else {
+		ouCert = ""
+	}
+
+	data, ok = tokenmap["Kubernetes API Server CA Certificate"]
+	if ok {
+		k8sCert = byte2string(data)
+	} else {
+		k8sCert = ""
+	}
+
+	fmt.Println(userName)
+	fmt.Println(k8sURL)
+	fmt.Println(ctxName)
+	fmt.Println(refreshToken)
+	fmt.Println(userIDToken)
+	fmt.Println(ouCert)
+	fmt.Println(k8sCert)
+
+	/*var kubeconfig *string
+	if home := homeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()*/
+
+	//config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+
+	pathOptions := clientcmd.NewDefaultPathOptions()
+	curCfg, err := pathOptions.GetStartingConfig()
+
+	cluster, ok := curCfg.Clusters[ctxName]
+	if !ok {
+		cluster = api.NewCluster()
+		curCfg.Clusters[ctxName] = cluster
+	}
+
+	cluster.Server = k8sURL
+	cluster.InsecureSkipTLSVerify = false
+
+	if len(k8sCert) == 0 {
+		cluster.CertificateAuthorityData = nil
+	} else {
+		cluster.CertificateAuthorityData = []byte(k8sCert)
+	}
+
+	authInfo, ok := curCfg.AuthInfos[userName]
+
+	if !ok {
+		authInfo = api.NewAuthInfo()
+		curCfg.AuthInfos[userName] = authInfo
+	}
+
+	authInfo.AuthProvider = &api.AuthProviderConfig{
+		Name:   "oidc",
+		Config: make(map[string]string),
+	}
+
+	authInfo.AuthProvider.Config["client-id"] = "kubernetes"
+	authInfo.AuthProvider.Config["id-token"] = userIDToken
+	authInfo.AuthProvider.Config["idp-certificate-authority-data"] = ouCert
+	authInfo.AuthProvider.Config["idp-issuer-url"] = "https://k8sou.apps.192-168-2-144.nip.io/auth/idp/k8sIdp"
+	authInfo.AuthProvider.Config["refresh-token"] = refreshToken
+
+	context, ok := curCfg.Contexts[ctxName]
+
+	if !ok {
+		context = api.NewContext()
+		curCfg.Contexts[ctxName] = context
+	}
+
+	context.AuthInfo = userName
+	context.Cluster = ctxName
+
+	curCfg.CurrentContext = ctxName
+
+	clientConfig := clientcmd.NewDefaultClientConfig(*curCfg, nil)
+
+	clientcmd.ModifyConfig(clientConfig.ConfigAccess(), *curCfg, false)
+
+	go oidcSvc.httpServer.Shutdown(oidcSvc.ctx)
+
+}
+
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
+}
+
+func byte2string(data []byte) string {
+	return (strings.Replace(strings.Replace(strings.Replace(string(data[1:len(data)-1]), "\\n", "\n", -1), "\\r", "", -1), "\\u003d", "=", -1))
+}
