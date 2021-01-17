b04e6b1ccb34cb155c91ade1c2752a317ed84fe2
mlbiam <marc@tremolo.io>
2021-01-16 21:33:57 -0500

    fix version text

--- kubectl-login.go
+++ kubectl-login.go
@@ -73,7 +73,7 @@ func randSeq(n int) string {
 }
 
 func main() {
-	fmt.Println("oulogin 0.0.4b2")
+	fmt.Println("oulogin 0.0.4")
 	host := flag.String("host", "", "openunison hostname (and port if needed)")
 
 	ctx := flag.String("context", "", "an existing context in the kubectl configuration file")
